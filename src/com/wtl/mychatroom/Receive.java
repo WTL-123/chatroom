652ac85e30905aaaef5a37226ee2e8ec4cffe424
WTL-123 <60888277+WTL-123@users.noreply.github.com>
2020-11-20 21:22:51 +0800

    Update Receive.java

--- src/com/wtl/mychatroom/Receive.java
+++ src/com/wtl/mychatroom/Receive.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 /**
- * 客户端接收消息类adasdsdasdadsadasd
+ * 客户端接收消息类
  */
 public class Receive implements Runnable {
     private DataInputStream dis;
